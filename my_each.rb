71d906b38e76581471ee157247bf462cc4638485
Michael Waldo <mbwcogic@yahoo.com>
2019-12-27 03:28:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+ i = 0 
+ while i < array.length
+ yeild array [i]
+
+  i= i + 1
+ # code here
+  end
 end
